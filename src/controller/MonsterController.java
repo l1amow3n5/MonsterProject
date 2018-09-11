5884ebd1107766b5e3c67103c838d7b7e5cabb46
Liam Owen <l1amow3n5@yahoo.com>
2018-09-11 12:13:33 -0600

    made my controller look at myMonster

--- src/controller/MonsterController.java
+++ src/controller/MonsterController.java
@@ -1,10 +1,20 @@
 package controller;
 
+import model.MarshmallowMonster; 
 public class MonsterController
 {
+	//Data member section
+	//Declaration section
+	private MarshmallowMonster myMonster;
+	
+	//Constructors initialize data members!
+	public MonsterController()
+	{
+		myMonster = new MarshmallowMonster();
+	}
 	public void start()
 	{
-		
+		System.out.println(myMonster);
 	}
 
 }
---
+++ src/model/MarshmallowMonster.java
@@ -0,0 +1,19 @@
+package model;
+
+public class MarshmallowMonster
+{
+	public MarshmallowMonster()
+	{
+		//Default constructor
+		//Generally boring and not as helpful
+		
+	}
+	
+	public String toString()
+	{
+		String description = "";
+				
+		return description;		
+	}
+
+}
